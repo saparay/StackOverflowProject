afee090522f69e23690cbf6ab9e59a1fc29e4c9c
saparay <saparay4@gmail.com>
2023-05-11 18:41:18 +0530

    service layer 1

---
+++ StackOverflowProject.ServiceLayer/UsersService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.Repositories;
+using StackOverflowProject.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public interface IUsersService
+    {
+
+    }
+    public class UsersService
+    {
+    }
+}
--- StackOverflowProject.ViewModels/VoteViewModel.cs
+++ StackOverflowProject.ViewModels/VoteViewModel.cs
@@ -13,6 +13,6 @@ namespace StackOverflowProject.ViewModels
         public int VoteID { get; set; }
         public int UserID { get; set; }
         public int AnswerID { get; set; }
-        public int VoteValue { get; set; }
+        public int VoteValue { get; set; }  
     }
 }
